879a068d868a89342473a4357f764b173f666c4b
rin4ik94 <rin4ik1994@gmail.com>
2018-10-10 18:58:59 +0500

    awdaw

--- routes/web.php
+++ routes/web.php
@@ -2,28 +2,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
-    Auth::routes();
-    
-    Route::group([
-        'namespace' => 'Admin',
-        'prefix' => 'admin',
-        'middleware' => 'auth'
-    ],function () {
+Auth::routes();
+
+Route::group([
+    'namespace' => 'Admin',
+    'prefix' => 'admin',
+    'middleware' => 'auth'
+], function () {
     Route::get('/dashboard', 'DashboardController@index');
-    Route::resource('roles','RoleController');
-    Route::resource('employees/group','EmployeegroupController',['as' => 'employees']);
-    Route::resource('employees','UserController');
+    Route::resource('roles', 'RoleController');
+    Route::resource('employees/group', 'awdawdEmployeegroupController', ['as' => 'employees']);
+    Route::resource('employees', 'UserController');
     Route::namespace('Managers')->group(function () {
-        Route::resource('managers/group','ManagerGroupController',['as' => 'managers']);
-        Route::resource('managers','ManagerController');
+        Route::resource('managers/group', 'ManagerGroupController', ['as' => 'managers']);
+        Route::resource('managers', 'ManagerController');
     });
-    Route::resource('branchs','BranchController');
-    Route::resource('orders','OrderController');
-    Route::resource('reports','ReportController');
-    Route::resource('couriers','CourierController');
-    Route::resource('categories','CategoryController');
-    Route::resource('clients/group','ClientgroupController',['as' => 'clients']);
-    Route::resource('clients','ClientController');
-    Route::resource('settings','SettingController');
-    Route::resource('products','ProductController');
+    Route::resource('branchs', 'BranchController');
+    Route::resource('orders', 'OrderController');
+    Route::resource('reports', 'ReportController');
+    Route::resource('couriers', 'CourierController');
+    Route::resource('categories', 'CategoryController');
+    Route::resource('clients/group', 'ClientgroupController', ['as' => 'clients']);
+    Route::resource('clients', 'ClientController');
+    Route::resource('settings', 'SettingController');
+    Route::resource('products', 'ProductController');
 });
