1b9be7f596c0182156219eb033583295729f6fa5
rin4ik94 <rin4ik1994@gmail.com>
2018-10-13 11:35:53 +0500

    fix styles

--- resources/views/front/desktop/layouts/css.blade.php
+++ resources/views/front/desktop/layouts/css.blade.php
@@ -1,6 +1,6 @@
 {{--
 <link rel="stylesheet" href="/css/app.css"> --}}
-<link rel="stylesheet" href="/desktop/css/bootstrap.css">
-<link rel="stylesheet" href="/desktop/css/grid.css">
+<link rel="stylesheet" href="/css/bootstrap.css">
+<link rel="stylesheet" href="/css/grid.css">
 <link rel="stylesheet" href="/desktop/css/mobile.css">
 <link rel="stylesheet" href="/desktop/css/style.css">
