64efb1f42ab7e6dff48645267b3565982ccacf6a
jkholmatov <jkholmatov@yandex.com>
2018-10-15 14:58:40 +0500

    'jnh'

--- resources/views/admin/managers/add.blade.php
+++ resources/views/admin/managers/add.blade.php
@@ -1,14 +1,12 @@
 <div class="modal fade" id="newStore" tabindex="-1" role="dialog" aria-hidden="true">
   <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
+    <form class="modal-content" action="{{route('managers.store')}}" method="POST" class="needs-validation" enctype="multipart/form-data" novalidate>
       <div class="modal-header">
         <h5 class="modal-title">Новый магазин</h5>
         <button type="button" class="close d-none" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <form action="{{route('managers.store')}}" method="POST" class="needs-validation" enctype="multipart/form-data" novalidate> 
-        @csrf
       <div class="modal-body">
         <div class="row">
           <div class="col-5">
@@ -47,10 +45,10 @@
         </div>
       </div>
       <div class="modal-footer">
+        @csrf
         <button type="button" class="btn btn-light" data-dismiss="modal">Закрыть</button>
         <button type="submit" class="btn btn-green">OK</button>
       </div>
-      </form>
-    </div>
+    </form>
   </div>
 </div>
