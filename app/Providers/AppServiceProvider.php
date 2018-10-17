c0777ae89427d095218a8ddf2136b0b3c3a01221
rin4ik94 <rin4ik1994@gmail.com>
2018-10-17 13:50:25 +0500

    fix child categories error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,13 @@
 <?php
 
 namespace App\Providers;
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\Paginator;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,7 +18,23 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        //
+        if (!Collection::hasMacro('paginate')) {
+
+            Collection::macro(
+                'paginate',
+                function ($perPage = 15, $page = null, $options = []) {
+                    $page = $page ? : (Paginator::resolveCurrentPage() ? : 1);
+                    return (new LengthAwarePaginator(
+                        $this->forPage($page, $perPage),
+                        $this->count(),
+                        $perPage,
+                        $page,
+                        $options
+                    ))
+                        ->withPath('');
+                }
+            );
+        }
     }
 
     /**
