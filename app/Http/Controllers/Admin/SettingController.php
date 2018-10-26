534dd2cb0b0aa397bb9e6ccaeb96b9a541d1fbfb
azizbek8434 <azizbek_muzaffarov@mail.ru>
2018-10-26 12:48:25 +0500

    admin settings changes

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -14,7 +14,8 @@ class SettingController extends Controller
      */
     public function index()
     {
-        return view('admin.settings.index');
+        $settings = config('dostavka');
+        return view('admin.settings.index',compact('settings'));
     }
 
     public function orders()
@@ -24,11 +25,38 @@ class SettingController extends Controller
 
     public function goto()
     {
-        return view('admin.settings.goto');
+        $delivery_price = config('dostavka.delivery_price');
+        return view('admin.settings.goto',compact('delivery_price'));
     }
 
     public function payment()
     {
         return view('admin.settings.payment');
+    }
+
+
+    public function update(Request $request)
+    {
+        if ($request->has('delivery_price')) {
+            config(['dostavka.delivery_price' => $request->delivery_price]);
+            $fp = fopen(base_path() . '/config/dostavka.php', 'w');
+            fwrite($fp, '<?php return ' . var_export(config('dostavka'), true) . ';');
+            fclose($fp);
+            \Artisan::call('config:cache');
+            return back();
+        }
+
+        if ($request->has('message') && $request->has('site_name')) {
+            config([
+            'dostavka.site_name' => $request->site_name,
+            'dostavka.message' => $request->message,
+            'dostavka.status' => $request->status ? 1 : 0,
+            ]);
+            $fp = fopen(base_path() . '/config/dostavka.php', 'w');
+            fwrite($fp, '<?php return ' . var_export(config('dostavka'), true) . ';');
+            fclose($fp);
+            \Artisan::call('config:cache');
+            return back();
+        }
     }
 }
--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -3,13 +3,15 @@
 <div class="main-top">
   <h1 class="main-title">Общие настройки</h1>
 </div>
-<form class="needs-validation" novalidate>
+<form class="needs-validation" action="/admin/settings/update" method="POST" novalidate>
+    @method('PATCH')
+    @csrf
   <div class="form-group form-row">
     <div class="col-3 col-form-label">
       <label class="form-label" for="">Название сайта</label>
     </div>
     <div class="col-9">
-      <input class="form-control" type="text"  required>
+      <input class="form-control" type="text" name="site_name" value="{{ $settings['site_name'] }}" required>
     </div>
   </div>
   <div class="form-group form-row">
@@ -18,7 +20,7 @@
     </div>
     <div class="col-9">
       <div class="custom-control custom-checkbox">
-      <input type="checkbox" class="custom-control-input" id="customCheck1">
+      <input type="checkbox" class="custom-control-input" name="status" id="customCheck1" @if($settings['status'] == 1) checked @endif>
       <label class="custom-control-label" for="customCheck1"></label>
     </div>
     </div>
@@ -28,8 +30,7 @@
       <label class="form-label" for="">Причина отключения сайта</label>
     </div>
     <div class="col-9">
-      <textarea class="form-control" rows="5">Сайт находится на текущей реконструкции, после завершения всех работ сайт будет открыт.
-  Приносим вам свои извинения за доставленные неудобства.</textarea>
+    <textarea class="form-control" rows="5" name="message" required>{{ $settings['message'] }}</textarea>
     </div>
   </div>
   <div class="form-row">
