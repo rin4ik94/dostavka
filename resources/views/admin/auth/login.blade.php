fcd2e575efe426bab22e84111acbde4d8edf9521
azizbek8434 <azizbek_muzaffarov@mail.ru>
2018-10-30 15:02:58 +0500

    changes

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
 <head>
 @include('admin.layouts.inc.head')
 </head>
 <body>
 <div class="app login">
-  <form class="form-login" method="POST" action="{{ route('admin.login') }}" aria-label="{{ __('Login') }}">
+  <form class="form-login" method="POST" action="{{ route('admin.login') }}" aria-label="Login">
   <div class="logo"></div>
   <h1 class="title">Личный кабинет</h1>
     <div class="card">
@@ -16,7 +16,7 @@
               <div class="input-group-prepend">
                 <span class="input-group-text" id="basic-addon1">+998</span>
               </div>
-              <input id="mobile" type="mobile" class="form-control {{ $errors->has('mobile') ? 'border-danger' : '' }}" name="mobile" value="{{ old('mobile') }}" required autofocus placeholder="{{ __('Mobile Number') }}">
+              <input id="mobile" type="mobile" class="form-control {{ $errors->has('mobile') ? 'border-danger' : '' }}" name="mobile" value="{{ old('mobile') }}" required autofocus placeholder="Mobile Number">
             </div>
             @if ($errors->has('mobile'))
             <span class="text-red">{{ $errors->first('mobile') }}</span>
@@ -31,10 +31,10 @@
           <div class="form-group">
             <div class="custom-control custom-checkbox">
               <input type="checkbox" class="custom-control-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-              <label class="custom-control-label" for="remember">{{ __('Remember Me') }}</label>
+              <label class="custom-control-label" for="remember">Remember Me</label>
             </div>
           </div>
-        <button class="btn btn-block btn-green" type="submit">{{ __('Login') }}</button>
+        <button class="btn btn-block btn-green" type="submit">Login</button>
       </div>
     </div>
   </form>
